79d21cef5b86c8d19e5ed12321a5d5990a591ed3
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-04-19 10:46:35 +0000

    load also nt files 	modified:   config-files/virtuoso_scripts/clean_upload.sql

--- config-files/virtuoso_scripts/clean_upload.sql
+++ config-files/virtuoso_scripts/clean_upload.sql
@@ -10,12 +10,14 @@ log_enable (2);
 
 # remove old data
 sparql drop silent graph <http://data.vlaanderen.be/id/dataset/vodap>;
+sparql drop silent graph <http://data.vlaanderen.be/id/dataset/default>;
 
 
 # schedule everything in the directory to be loaded
 #  the last argument is a default value in case the .graph files are not present
 ld_dir('/data/dumps', '*.ttl.gz', 'http://data.vlaanderen.be/id/dataset/default');
 ld_dir('/data/dumps', '*.ttl', 'http://data.vlaanderen.be/id/dataset/default');
+ld_dir('/data/dumps', '*.nt', 'http://data.vlaanderen.be/id/dataset/default');
 
 # start loading
 rdf_loader_run();
