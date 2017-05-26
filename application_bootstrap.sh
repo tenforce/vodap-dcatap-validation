8b4715b8e54a4d0970bd9995fe3105da0f6fa472
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-05-26 11:35:01 +0000

    add the emacs support 	modified:   application_bootstrap.sh 	new file:   webservice/scripts/org-export

--- application_bootstrap.sh
+++ application_bootstrap.sh
@@ -8,6 +8,10 @@ apt-get install -y rasqal-utils raptor2-utils gnuplot
 
 # a tool to generate the html pages
 git clone https://github.com/binarin/docker-org-export.git 
+   # copy the configuration to the webservice directory
+   mkdir -p webservice/emacs
+   cp docker-org-export/*.el webservice/emacs
+   cp docker-org-export/.blockdiagrc webservice/emacs
 
 # the generic DCAT-AP queries
 git clone --depth=1 https://github.com/EmidioStani/dcat-ap_validator ISArules
