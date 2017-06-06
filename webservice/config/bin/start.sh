4c0a5249534f9503a723dc1c69c9a9b38e8da8d3
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-06-06 15:16:46 +0000

    adjust to the right filename 	modified:   bin/start.sh

--- webservice/config/bin/start.sh
+++ webservice/config/bin/start.sh
@@ -4,7 +4,7 @@
 /config/bin/replace-env.sh /scripts/validate.sh
 /config/bin/replace-env.sh /scripts/rdf_validate_url.sh
 /config/bin/replace-env.sh /scripts/dcat_validate.sh
-/config/bin/replace-env.sh /scripts/load_feed.sh
+/config/bin/replace-env.sh /scripts/load_feeds.sh
 
 touch /logs/webservice.log
 chmod 666 /logs/webservice.log
