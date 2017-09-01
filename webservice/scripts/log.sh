3794cd84606dc9453c4eb9c3159ddfd5f4ee7f62
Paul Massey <paul.massey@tenforce.com>
2017-09-01 16:19:44 +0200

    Additional comments, etc.

--- webservice/scripts/log.sh
+++ webservice/scripts/log.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+####################################################################################
+# title:       log.sh
+# description: make sure that the log messages contain the name of the
+#              script which issues them
+# 
 
 scriptname=$(basename $0)
-
 function log() {
     ( echo -n "[" ; echo -n `date` ; echo -n "] " ; echo $scriptname $* ) >> /logs/webservice.log
 }
+
+####################################################################################
+####################################################################################
